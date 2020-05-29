37ba90a8d638373e916b3c5082c15853068a1ff6
ImSupporter <adam990901@gmail.com>
2020-05-29 11:47:26 +0900

    AirCubeDrawing 프로젝트 추가
    ARCore menifest
    Camera Permission

---
+++ AirCubeDrawing/app/src/main/java/com/taehyun/aircubedrawing/Activity/MainActivity.java
@@ -0,0 +1,225 @@
+package com.taehyun.aircubedrawing.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.taehyun.aircubedrawing.R;
+import com.taehyun.aircubedrawing.Renderer.BackgroundRenderer;
+import com.taehyun.aircubedrawing.Renderer.CubeRenderer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity implements GLSurfaceView.Renderer{
+
+    private boolean mUserRequestedInstall = false;
+    private boolean mViewportChanged = false;
+    private int mViewportWidth = -1;
+    private int mViewportHeight = -1;
+
+    //카메라 권한
+    private String[] REQUIRED_PERMISSSIONS = {Manifest.permission.CAMERA};
+    private final int PERMISSION_REQUEST_CODE = 0; // PROTECTION_NORMAL
+
+    private GLSurfaceView glView;
+    BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
+    private Session session;
+    private Frame frame;
+
+    private BlockingQueue<MotionEvent> tapQueue = new ArrayBlockingQueue<>(16);
+    private long LastTapTime = 0;
+    private ArrayList<float[]> cubePoses = new ArrayList<>(20);
+
+    private CubeRenderer cubeRenderer;
+
+    private float[] viewMatrix = new float[16];
+    private float[] projMatrix = new float[16];
+    private float[] modelMatrix = new float[16];
+    private float[] vpMatrix = new float[16];
+
+    private int angle = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        glView = findViewById(R.id.glView);
+        glView.setPreserveEGLContextOnPause(true);
+        glView.setEGLContextClientVersion(2);
+        glView.setEGLConfigChooser(8,8,8,8,16,0);
+        glView.setRenderer(this);
+        glView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+        for(String permission : REQUIRED_PERMISSSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSSIONS, PERMISSION_REQUEST_CODE);
+            }
+        }
+
+        glView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(), "Touched : " + cubePoses.size(), Toast.LENGTH_SHORT).show();
+                tapQueue.offer(event);
+                return false;
+            }
+        });
+
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(session != null){
+            glView.onPause();
+            session.pause();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(session == null){
+            try{
+                switch(ArCoreApk.getInstance().requestInstall(this,!mUserRequestedInstall)){
+                    case INSTALL_REQUESTED:
+                        mUserRequestedInstall = true;
+                        return;
+                    case INSTALLED:
+                        break;
+                }
+                session = new Session(this);
+
+                Config config = new Config(session);
+                config.setLightEstimationMode(Config.LightEstimationMode.DISABLED);
+                config.setPlaneFindingMode(Config.PlaneFindingMode.DISABLED);
+                config.setFocusMode(Config.FocusMode.AUTO);
+                session.configure(config);
+
+            }catch (Exception e){
+                Log.d("ULTRA", e.getMessage());
+                return;
+            }
+        }
+
+        try{
+            session.resume();
+        }catch (CameraNotAvailableException e){
+            e.printStackTrace();
+            session = null;
+            finish();
+        }
+
+        glView.onResume();
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        GLES20.glDisable(GLES20.GL_CULL_FACE);
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        try{
+            backgroundRenderer.createOnGlThread(this);
+            cubeRenderer = new CubeRenderer();
+        }catch (IOException e){
+            e.getMessage();
+        }
+    }
+
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        mViewportChanged = true;
+        mViewportWidth = width;
+        mViewportHeight = height;
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT|GLES20.GL_DEPTH_BUFFER_BIT);
+
+        if(session == null){
+            return;
+        }
+        if(mViewportChanged){
+            int displayRotation = getWindowManager().getDefaultDisplay().getRotation();
+            session.setDisplayGeometry(displayRotation, mViewportWidth, mViewportHeight);
+        }
+        try{
+            session.setCameraTextureName(backgroundRenderer.getTextureId());
+            frame = session.update();
+            Camera camera = frame.getCamera();
+            backgroundRenderer.draw(frame);
+
+            if(camera.getTrackingState() == TrackingState.TRACKING){
+                camera.getViewMatrix(viewMatrix, 0);
+                camera.getProjectionMatrix(projMatrix, 0, 0.1f, 100.0f);
+                Matrix.multiplyMM(vpMatrix, 0, projMatrix,0,viewMatrix,0);
+
+                Matrix.setIdentityM(modelMatrix, 0);
+                Matrix.rotateM(modelMatrix, 0, angle,1,1,1);
+                angle++;
+                for(float[] t : cubePoses){
+                    float[] temp = new float[16];
+                    Matrix.setIdentityM(temp, 0);
+                    Matrix.translateM(temp, 0, t[0], t[1], t[2]);
+
+                    Matrix.multiplyMM(temp,0, temp,0, modelMatrix, 0);
+
+                    Matrix.multiplyMM(temp,0,vpMatrix,0, temp,0);
+                    cubeRenderer.draw(temp);
+                }
+            }
+            handleTap();
+        }catch (CameraNotAvailableException e){
+            finish();
+        }
+    }
+
+    public void handleTap(){
+        MotionEvent tap = tapQueue.poll();
+        if(tap == null || LastTapTime == tap.getEventTime()) return;
+
+        if(cubePoses.size() == 20){
+            cubePoses.remove(0);
+        }
+
+        cubePoses.add(new float[]{
+                (float) (frame.getCamera().getPose().tx() + (float)frame.getCamera().getPose().getZAxis()[0] * -0.3),
+                (float) (frame.getCamera().getPose().ty() + (float)frame.getCamera().getPose().getZAxis()[1] * -0.3),
+                (float) (frame.getCamera().getPose().tz() + (float)frame.getCamera().getPose().getZAxis()[2] * -0.3),
+        });
+        LastTapTime = tap.getEventTime();
+    }
+}
---
+++ AirCubeDrawing/app/src/main/java/com/taehyun/aircubedrawing/Renderer/CubeRenderer.java
@@ -0,0 +1,136 @@
+package com.taehyun.aircubedrawing.Renderer;
+
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+public class CubeRenderer {
+    int BYTE_PER_FLOAT = 4;
+    int COORDS_PER_VERTEX = 3;
+    int vertexStride = BYTE_PER_FLOAT * COORDS_PER_VERTEX;
+
+    private FloatBuffer vertexBuffer;
+    private FloatBuffer colorBuffer;
+    private ShortBuffer indexBuffer;
+    int vertexShader, fragmentShader, mProgram;
+
+    int mPositionHandle;
+    int mColorHandle;
+    int mMVPMatrixHandle;
+    
+    float scale = 0.03f;
+
+    public float [] vertex={
+            -scale, -scale, -scale,
+            scale, -scale, -scale,
+            scale, scale, -scale,
+            -scale, scale, -scale,
+            -scale, -scale, scale,
+            scale, -scale, scale,
+            scale, scale, scale,
+            -scale, scale, scale
+    };
+    public short[] vIndex={
+            0, 1, 3, 3, 1, 2, // Front face.
+            0, 1, 4, 4, 5, 1, // Bottom face.
+            1, 2, 5, 5, 6, 2, // Right face.
+            2, 3, 6, 6, 7, 3, // Top face.
+            3, 7, 4, 4, 3, 0, // Left face.
+            4, 5, 7, 7, 6, 5, // Rear face.
+    };
+
+    public float[] vColor={
+            0.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f, 1.0f,
+            0.0f, 1.0f, 1.0f, 1.0f
+    };
+
+    public final String vertexShaderCode =
+            "attribute vec4 vPosition;" +
+            "uniform mat4 uMVPMatrix;" +
+            "varying vec4 vColor;" +
+            "attribute vec4 aColor;" +
+            "void main(){" +
+            "   gl_Position = uMVPMatrix * vPosition;" +
+            "   vColor = aColor;" +
+            "}";
+    public final String fragmentShaderCode =
+            "precision mediump float;" +
+            "varying vec4 vColor;" +
+            "void main(){" +
+            "   gl_FragColor = vColor;" +
+            "}";
+
+    public static int loadShader(int type, String shaderCode){
+        int shader = GLES20.glCreateShader(type);
+
+        GLES20.glShaderSource(shader,shaderCode);
+
+        GLES20.glCompileShader(shader);
+        return shader;
+    }
+
+    public CubeRenderer(){
+        ByteBuffer bb = ByteBuffer.allocateDirect(vertex.length * BYTE_PER_FLOAT);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(vertex);
+        vertexBuffer.position(0);
+
+        ByteBuffer cbb = ByteBuffer.allocateDirect(vColor.length * BYTE_PER_FLOAT);
+        cbb.order(ByteOrder.nativeOrder());
+        colorBuffer = cbb.asFloatBuffer();
+        colorBuffer.put(vColor);
+        colorBuffer.position(0);
+
+        ByteBuffer ibb = ByteBuffer.allocateDirect(vIndex.length * 2);
+        ibb.order(ByteOrder.nativeOrder());
+        indexBuffer = ibb.asShortBuffer();
+        indexBuffer.put(vIndex);
+        indexBuffer.position(0);
+
+        vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        mProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+
+        GLES20.glLinkProgram(mProgram);
+    }
+    public void draw(float[] mvpMatrix){
+        GLES20.glUseProgram(mProgram);
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        //triangle vertex 속성을 활성화 시켜야 렌더링시 반영되서 그려짐
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // triangle vertex 속성을 vertexBuffer에 저장되어 있는 vertex 좌표들로 정의한다.
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+
+
+        // program 객체로부터 fragment shader의 vColor 멤버에 대한 핸들을 가져옴
+        mColorHandle = GLES20.glGetAttribLocation(mProgram, "aColor");
+
+        //triangle 렌더링시 사용할 색으로 color변수에 정의한 값을 사용한다.
+        GLES20.glEnableVertexAttribArray(mColorHandle);
+        GLES20.glVertexAttribPointer(mColorHandle, 4, GLES20.GL_FLOAT, false, 16, colorBuffer);
+
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix,0);
+
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, vIndex.length , GLES20.GL_UNSIGNED_SHORT, indexBuffer);
+
+        //vertex 속성을 비활성화 한다.
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+        GLES20.glDisableVertexAttribArray(mColorHandle);
+    }
+}
